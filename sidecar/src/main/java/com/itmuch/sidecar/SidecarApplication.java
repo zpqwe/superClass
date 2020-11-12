15411f1457f62725a1c441bafd75f0917b993c29
zp <zp@qq.com>
2020-11-12 15:56:42 +0800

    微服务与异构微服务完美调用

---
+++ sidecar/src/main/java/com/itmuch/sidecar/SidecarApplication.java
@@ -0,0 +1,13 @@
+package com.itmuch.sidecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidecarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SidecarApplication.class, args);
+    }
+
+}
---
+++ sidecar/src/test/java/com/itmuch/sidecar/SidecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmuch.sidecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidecarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
